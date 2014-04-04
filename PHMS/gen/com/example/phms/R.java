983d2c3159a7be69366e81e5737f8347e53e7b75
seth-skocelas <s.skocelas@gmail.com>
2014-04-04 15:58:31 -0500

    Revert "Initial Upload"
    This reverts commit 4d54036e1c97bf2f299e5188364c7ec80e2f867c.
    
    Conflicts:
    	PHMS/bin/classes/com/example/phms/MainMenuActivity$1.class
    	PHMS/bin/classes/com/example/phms/MainMenuActivity$2.class
    	PHMS/bin/classes/com/example/phms/MainMenuActivity.class
    	PHMS/res/layout/activity_main_menu.xml

--- PHMS/gen/com/example/phms/R.java
+++ PHMS/gen/com/example/phms/R.java
@@ -24,51 +24,58 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080020;
-        public static final int button1=0x7f080008;
-        public static final int button2=0x7f080007;
-        public static final int button3=0x7f080006;
-        public static final int button4=0x7f080009;
-        public static final int button5=0x7f08000a;
-        public static final int button6=0x7f08000b;
-        public static final int button7=0x7f08000c;
-        public static final int editText1=0x7f080004;
-        public static final int editText10=0x7f08001d;
-        public static final int editText11=0x7f08001f;
-        public static final int editText12=0x7f08001e;
-        public static final int editText2=0x7f080005;
-        public static final int editText3=0x7f080015;
-        public static final int editText4=0x7f080016;
-        public static final int editText5=0x7f080017;
-        public static final int editText6=0x7f080018;
-        public static final int editText7=0x7f080019;
-        public static final int editText8=0x7f08001b;
-        public static final int editText9=0x7f08001a;
-        public static final int textView1=0x7f080000;
-        public static final int textView10=0x7f080010;
-        public static final int textView11=0x7f08000f;
-        public static final int textView12=0x7f08000d;
-        public static final int textView13=0x7f08001c;
-        public static final int textView2=0x7f080001;
-        public static final int textView3=0x7f080002;
-        public static final int textView4=0x7f080003;
-        public static final int textView5=0x7f080014;
-        public static final int textView6=0x7f080013;
-        public static final int textView7=0x7f080012;
-        public static final int textView8=0x7f080011;
-        public static final int textView9=0x7f08000e;
+        public static final int action_settings=0x7f080025;
+        public static final int button1=0x7f080005;
+        public static final int button2=0x7f080006;
+        public static final int button3=0x7f08000d;
+        public static final int button4=0x7f08000e;
+        public static final int button5=0x7f08000f;
+        public static final int button6=0x7f080010;
+        public static final int button7=0x7f080011;
+        public static final int editText1=0x7f08000b;
+        public static final int editText10=0x7f080022;
+        public static final int editText11=0x7f080024;
+        public static final int editText12=0x7f080023;
+        public static final int editText2=0x7f08000c;
+        public static final int editText3=0x7f08001a;
+        public static final int editText4=0x7f08001b;
+        public static final int editText5=0x7f08001c;
+        public static final int editText6=0x7f08001d;
+        public static final int editText7=0x7f08001e;
+        public static final int editText8=0x7f080020;
+        public static final int editText9=0x7f08001f;
+        public static final int linearLayout1=0x7f080002;
+        public static final int listView1=0x7f080003;
+        public static final int listView2=0x7f080004;
+        public static final int suggestedCalorie=0x7f080000;
+        public static final int textView1=0x7f080007;
+        public static final int textView10=0x7f080015;
+        public static final int textView11=0x7f080014;
+        public static final int textView12=0x7f080012;
+        public static final int textView13=0x7f080021;
+        public static final int textView2=0x7f080008;
+        public static final int textView3=0x7f080009;
+        public static final int textView4=0x7f08000a;
+        public static final int textView5=0x7f080019;
+        public static final int textView6=0x7f080018;
+        public static final int textView7=0x7f080017;
+        public static final int textView8=0x7f080016;
+        public static final int textView9=0x7f080013;
+        public static final int todaysCalorie=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main_menu=0x7f030001;
-        public static final int activity_recover=0x7f030002;
-        public static final int activity_register=0x7f030003;
+        public static final int activity_diet=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main_menu=0x7f030002;
+        public static final int activity_recover=0x7f030003;
+        public static final int activity_register=0x7f030004;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int main_menu=0x7f070001;
-        public static final int recover=0x7f070002;
-        public static final int register=0x7f070003;
+        public static final int diet=0x7f070000;
+        public static final int login=0x7f070001;
+        public static final int main_menu=0x7f070002;
+        public static final int recover=0x7f070003;
+        public static final int register=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -101,6 +108,7 @@ public final class R {
         public static final int register=0x7f050007;
         public static final int search_app=0x7f050023;
         public static final int security=0x7f050016;
+        public static final int title_activity_diet=0x7f050027;
         public static final int title_activity_main_menu=0x7f05001d;
         public static final int title_activity_medication=0x7f050026;
         public static final int title_activity_recover=0x7f050019;
