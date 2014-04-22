370efce3b9ea945a128a2e003e7420c65df7fb4e
lammypham <phamlam92@gmail.com>
2014-04-22 03:46:16 -0500

    Lam- changes to alarm

--- PHMS/src/com/example/phms/AddAlertActivity.java
+++ PHMS/src/com/example/phms/AddAlertActivity.java
@@ -1,17 +1,15 @@
+
 package com.example.phms;
 
-import java.io.IOException;
+import java.util.Calendar;
 
 import android.app.Activity;
 import android.app.AlarmManager;
-import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -29,6 +27,8 @@ public class AddAlertActivity extends Activity {
 	private View promptsView;
 	private int selectHours;
 	private int selectMinutes;
+	private int hour;
+	private int min;
 	
 	
 	// opens add_alert xml
@@ -50,34 +50,45 @@ public class AddAlertActivity extends Activity {
 		fri = (CheckBox) findViewById(R.id.Friday);
 		sat = (CheckBox) findViewById(R.id.Saturday);
 
+		Calendar calendar = Calendar.getInstance();
+		hour = time_picker.getCurrentHour();
+		min = time_picker.getCurrentMinute();
+        calendar.set(Calendar.HOUR_OF_DAY,hour);
+        calendar.set(Calendar.MINUTE, min);
+        calendar.set(Calendar.SECOND, 0);
+		
+	
 		
 		
 		setButton.setOnClickListener(new OnClickListener()
 		{
-			public void onClick(View arg0)
+			public void onClick(View arg0)						
 			{
-				
-					//Toast.makeText(AddAlertActivity.this, ""+selectHours+":"+selectMinutes, Toast.LENGTH_LONG).show();
-					Intent intent = new Intent(AddAlertActivity.this, AlarmReceiverActivity.class);
-					PendingIntent pi = PendingIntent.getActivity(AddAlertActivity.this, 2, 
-							intent, PendingIntent.FLAG_CANCEL_CURRENT);
-					AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
-					am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+ 5*1000, pi); 
-					// the time is 5 seconds in type long
-					//use for test
+					//Toast.makeText(AddAlertActivity.this, " "+time_picker.getCurrentHour()+" "+time_picker.getCurrentMinute(),Toast.LENGTH_LONG).show();
+					//Calendar cal = Calendar.getInstance();
 					
-					if (mToast != null)
+					//cal.set(2014,4,15, time_picker.getCurrentHour(), time_picker.getCurrentMinute(), 00);
+					Calendar calendar = Calendar.getInstance();
+					hour = time_picker.getCurrentHour();
+					min = time_picker.getCurrentMinute();
+			        calendar.set(Calendar.HOUR_OF_DAY,hour);
+			        calendar.set(Calendar.MINUTE, min);
+			        calendar.set(Calendar.SECOND, 0);
+					//when they choose to repeat just once
+					if(sun.isChecked() == false && mon.isChecked()== false &&tues.isChecked()==false &&wed.isChecked()==false
+							&&thur.isChecked()==false && fri.isChecked()==false && sat.isChecked()==false)
 					{
-						mToast.cancel();
+						setAlarm(calendar);
+						Toast.makeText(AddAlertActivity.this, " "+time_picker.getCurrentHour()+" "+time_picker.getCurrentMinute(),Toast.LENGTH_LONG).show();
+						
 					}
 					
-					mToast = Toast.makeText(getApplicationContext(),
-							"Alarm Activity set in:"+5+" seconds",
-							Toast.LENGTH_LONG);
-					mToast.show();
-				
-	
-				
+					
+					else
+					{
+						//this is where if the days they want to repeat are checked
+						
+					}			
 			}
 		});
 		
@@ -90,8 +101,21 @@ public class AddAlertActivity extends Activity {
 			}
 		});
 		
-		
 	}
+	 private void setAlarm(Calendar targetCal){
+
+		// Toast.makeText(AddAlertActivity.this, " "+targetCal,Toast.LENGTH_LONG).show();
+		 /* mToast.makeText(getApplicationContext(),
+					"Alarm Activity set in:"+" seconds",
+					Toast.LENGTH_LONG);*/
+		  long time = targetCal.getTimeInMillis();
+		  Intent intent = new Intent(AddAlertActivity.this, AlarmReceiverActivity.class);
+			PendingIntent pi = PendingIntent.getActivity(AddAlertActivity.this, 1, 
+					intent, PendingIntent.FLAG_CANCEL_CURRENT);
+			AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
+			am.set(AlarmManager.RTC_WAKEUP, time, pi); 
+
+		 }
 	
 	public void cancel(View view) {
 		Intent intent = new Intent(this, AlertActivity.class);
@@ -104,46 +128,6 @@ public class AddAlertActivity extends Activity {
 		getMenuInflater().inflate(R.menu.diet, menu);
 		return true;
 	}
-	
-    @Override
-  	public boolean onOptionsItemSelected(MenuItem item){
-    	// same as using a normal menu
-    	switch(item.getItemId()) {
-    	case R.id.action_settings:
-    		Intent intent = new Intent(this, SettingsActivity.class);
-    	    startActivity(intent);
-    		break;
-    	case R.id.action_search:
-    		Intent intent2 = new Intent(this, SearchActivity.class);
-    	    startActivity(intent2);
-    		break;
-    	case R.id.action_logout:
-    		
-    		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-    		alertDialog.setTitle("Logout");
-    		alertDialog.setMessage("Are you sure you want to logout?");
-    		alertDialog.setButton(-1,"OK", new DialogInterface.OnClickListener() {
-    		      public void onClick(DialogInterface dialog, int which) {
-    		    	  
-    		    	  Intent i = getBaseContext().getPackageManager()
-    		    	             .getLaunchIntentForPackage( getBaseContext().getPackageName() );
-    		    	i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    		    	startActivity(i);
-    		 
-    		    } });
-    		alertDialog.setButton(-2,"Cancel", new DialogInterface.OnClickListener() {
-    		      public void onClick(DialogInterface dialog, int which) {
-    		 
-
-    		    	  
-    		    } });
-    		alertDialog.show();
-    		break;
-    	
-    	}
-    	
-  		return true;
-  	}
 	
 	public void setTime(View view){
 		//create radio group buttons for when to repeat alarm
--- PHMS/src/com/example/phms/RepeatingAlarmActivity.java
+++ PHMS/src/com/example/phms/RepeatingAlarmActivity.java
@@ -1,16 +1,39 @@
+//repeatingalarm.java
 package com.example.phms;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class RepeatingAlarmActivity extends Activity {
+	private MediaPlayer mMediaPlayer;
+	private PowerManager.WakeLock mWakeLock;
+	
 	
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "My Wake Log");
+		mWakeLock.acquire();
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+				| WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+				| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON, 
+				 WindowManager.LayoutParams.FLAG_FULLSCREEN
+				 | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+					| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+				);
+		setContentView(R.layout.activity_alarm_screen);
 		setContentView(R.layout.activity_alarm_screen);
 		
 Button stopAlarm = (Button) findViewById(R.id.btnStop);
@@ -18,10 +41,30 @@ Button stopAlarm = (Button) findViewById(R.id.btnStop);
 		stopAlarm.setOnClickListener(new OnClickListener(){
 			public void onClick(View arg0)
 			{
-				//mMediaPlayer.stop();
+				mMediaPlayer.stop();
 				finish();
 			}
 		});
+	}
+	private Uri getAlarmUri()
+	{
+		Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+		if (alert ==null)
+		{
+			alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			if (alert == null)
+			{
+				alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+			}
+		}
+		
+		return alert;
+	}
+	
+	protected void onStop()
+	{
+		super.onStop();
+		mWakeLock.release();
 	}
 
 }
